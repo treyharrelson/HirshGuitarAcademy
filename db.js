c3c0b3ce836a613794b47bffdc695c289442d57e
mkenny904 <mlk040@email.latech.edu>
2026-01-03 15:11:31 -0600

    Set up server.js to use sequelize
    swapped out all sql logic with sequelize, didn't add any new functionality yet. All sql queries go through the models which use db.js. Should be pretty good. Also changed user_dashboard to student_dashboard just because I have the three roles as student, instructor, admin, we can change anything and everything if needed. Just trying to get a framework.

--- db.js
+++ db.js
@@ -2,20 +2,24 @@ require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres'
-  }
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres'
+    }
 );
 
 sequelize.authenticate().then(() => {
-	console.log('Connection has been established successfully.');
+    console.log('Connection has been established successfully.');
+    // make sure tables exist
+    sequelize.sync({ alter: true }).then(() => {
+        console.log('Tables synced successfully.');
+    });
 }).catch(err => {
-	console.error('Unable to connect to the database:', err);
+    console.error('Unable to connect to the database:', err);
 });
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const db = require("./db");
 const session = require("express-session");
 
 const router = express.Router();
+
+// SQL Models for sequelize
+// All SQL queries should go through here, e.g. 
+// Models.User.create(...), Models.User.findOne(...), etc.
+const Models = require('./models');
 
 const path = require('path');
 const app = express();
 const port = 3000;
 
 // Collect data sent from client
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,79 +25,107 @@ app.use(session({
   secret: "fortestingpurposes", // used to sign the session id cookie
   resave: false, // prevents the session from being saved back to the session store
   saveUninitialized: false, // prevents a asession from being saved if it hasnt been modified
-  cookie: {maxAge: 1000 * 60 * 60 * 24} // cookie expiration time
+  cookie: { maxAge: 1000 * 60 * 60 * 24 } // cookie expiration time
 }))
 
 // Register a new user
 app.post("/register", async (req, res) => {
-    const { name, email, password, role } = req.body;
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashedPassword;
 
-    try {
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
+    // should probably have more validation here
 
-        // Insert the new user into the database
-        const query = "INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)";
-        db.query(query, [name, email, hashedPassword, role], (err, result) => {
-            if (err) throw err;
-            res.status(201).send("User registered successfully");
-        });
-        res.redirect("public/index.html");
-    } catch (error) {
-        res.status(500).send("Error registering user");
-    }
+    // Insert the new user into the database
+    const newUser = await Models.User.create(req.body);
+    res.redirect("/index.html");
+  } catch (error) {
+    res.status(500).send(`Error registering user: ${error}`);
+  }
 });
 
 // User login
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
 
+  try {
     // Find the user by email
-    const query = 'SELECT * FROM users WHERE email = ?';
-    db.query(query, [email], async (err, results) => {
-        if (err) throw err;
+    const user = await Models.User.findOne({ where: { email: email } });
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    
 
-        if (results.length > 0) {
-            const user = results[0];
+    // Compare the hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      req.session.user = {
+        id: user._user_id,
+        role: user.role,
+        name: user.name,
+        email: user.email
+      };
+      // Redirect based on privilage/role
+      if (user.role === 'student') {
+        res.redirect('student_dashboard.html');
+      } else if (user.role === 'admin') {
+        res.redirect('admin_dashboard.html');
+      }
+    }
+    else {
+      res.status(401).send('Invalid credentials');
+    }
+  } catch (error) {
+    res.status(500).send(`Error logging in: ${error}`);
+  }
 
-            // Compare the hashed password
-            const isMatch = await bcrypt.compare(password, user.password);
 
-            if (isMatch) {
-                //res.status(200).send('Login successful');
 
-                //Create session data
-                req.session.user = {
-                  id: user._user_id,
-                  role: user.role,
-                  name: user.name,
-                  email: user.email
-                };
+  // db.query(query, [email], async (err, results) => {
+  //   if (err) throw err;
 
-                // Redirect based on privilage/role
-                if(user.role === 'user'){
-                  res.redirect('user_dashboard.html');
-                } else if (user.role === 'admin'){
-                  res.redirect('admin_dashboard.html');
-                }
+  //   if (results.length > 0) {
+  //     const user = results[0];
 
-            } else {
-                res.status(401).send('Invalid credentials');
-            }
-        } else {
-            res.status(404).send('User not found');
-        }
-    });
+  //     // Compare the hashed password
+  //     const isMatch = await bcrypt.compare(password, user.password);
+
+  //     if (isMatch) {
+  //       //res.status(200).send('Login successful');
+
+  //       //Create session data
+  //       req.session.user = {
+  //         id: user._user_id,
+  //         role: user.role,
+  //         name: user.name,
+  //         email: user.email
+  //       };
+
+  //       // Redirect based on privilage/role
+  //       if (user.role === 'user') {
+  //         res.redirect('user_dashboard.html');
+  //       } else if (user.role === 'admin') {
+  //         res.redirect('admin_dashboard.html');
+  //       }
+
+  //     } else {
+  //       res.status(401).send('Invalid credentials');
+  //     }
+  //   } else {
+  //     res.status(404).send('User not found');
+  //   }
+  // });
 });
 
 // User logout
 app.post('/logout', (req, res) => {
-  req.session.destroy(function(err){
-    if (err){
+  req.session.destroy(function (err) {
+    if (err) {
       return nextTick(err);
     }
     res.redirect("/");
-  }); 
+  });
 });
 
 module.exports = router;
